5d03d9e423d2d6e8276bf9deb09685381477a49b
ChonkySeeal <gsi0919@naver.com>
2023-04-27 00:03:10 +0900

    more dynamic contents in electron

--- renderer.js
+++ renderer.js
@@ -1,22 +1,10 @@
 
 
 
-const { ipcRenderer } = require('electron')
-console.log("am I being read?????????????")
-  ipcRenderer.on('attach-device', (sender, message) => {
-    console.log("testes")
-    const messageSpan = document.getElementById('message')
-    messageSpan.innerText = message.message
-  })
-ipcRenderer.on('remove-device', (sender, message) => {
-    console.log("testes")
-    const messageSpan = document.getElementById('message')
-    messageSpan.innerText = message.message
-  })
-ipcRenderer.on('card', (sender, message) => {
-    console.log("testes")
-    const messageSpan = document.getElementById('message')
-    const cardSpan = document.getElementById('card')
-    messageSpan.innerText = message.message
-    cardSpan.innerText = message.card.uid
-  })
+
+
+
+electronAPI.ScanCallBack((_event, value) => {
+    
+})
+
