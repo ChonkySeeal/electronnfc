e5451290d7668ad054fa494fb74cb091ce47fcf7
권성준 <gsj0919@pinnode.com>
2023-07-05 17:14:25 +0900

    "완성"

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ const { NFC } = require("nfc-pcsc");
 // const bookDb = "7706d45e1e6449fc86baf828633231c6";
 
 const notion = new Client({
-  auth: "secret_3ovPS05h7kYw14G5n9P7yKum6wvc4h7nc2LtVpnS2Gv",
+	auth: "secret_lj8NMQpObd8kZLp0eABz9O3vvr6XtLpzLqa37a0Xz1s",
 });
 
-const personDb = "4e9dae70b5f64f11ac02cb8186e62bbd";
-const bookDb = "7706d45e1e6449fc86baf828633231c6";
+const personDb = "84179885bbc8434e89441ec56313c014";
+const bookDb = "c8627e1066374f04a23f4e780c0c5b1e";
 let mainWindow;
 
 const nfc = new NFC();
@@ -29,224 +29,224 @@ let timerId;
 let personId;
 
 function resetEverything() {
-  bookObj.id = "";
-  bookObj.result = {};
-  personId = "";
-  timerId = "";
-  nfcObj.nfc = "";
-  nfcObj.condition = "";
+	bookObj.id = "";
+	bookObj.result = {};
+	personId = "";
+	timerId = "";
+	nfcObj.nfc = "";
+	nfcObj.condition = "";
 }
 
 function resetUI() {
-  timerId = setTimeout(() => {
-    mainWindow.webContents.send("reset");
-    resetEverything();
-  }, 20000);
+	timerId = setTimeout(() => {
+		mainWindow.webContents.send("reset");
+		resetEverything();
+	}, 20000);
 }
 
 function cancelResetEverything() {
-  clearTimeout(timerId);
+	clearTimeout(timerId);
 }
 
 ipcMain.on("borrow", (event, dto) => {
-  nfcObj.condition = "borrow";
-  resetUI();
+	nfcObj.condition = "borrow";
+	resetUI();
 });
 
 ipcMain.on("borrower", (event, dto) => {
-  nfcObj.condition = "borrower";
-  if (timerId != "") cancelResetEverything();
-  resetUI();
+	nfcObj.condition = "borrower";
+	if (timerId != "") cancelResetEverything();
+	resetUI();
 });
 
 ipcMain.on("return", (event, dto) => {
-  nfcObj.condition = "return";
-  resetUI();
+	nfcObj.condition = "return";
+	resetUI();
 });
 
 ipcMain.on("cancel", (event, dto) => {
-  mainWindow.webContents.send("reset");
-  resetEverything();
+	mainWindow.webContents.send("reset");
+	resetEverything();
 });
 
 nfc.on("reader", (reader) => {
-  reader.on("card", (card) => {
-    nfcObj.nfc = card.uid;
-    if (nfcObj.condition == "borrow") {
-      (async () => {
-        await notion.databases
-          .query({
-            database_id: bookDb,
-            filter: {
-              property: "NFC",
-              rich_text: {
-                equals: nfcObj.nfc,
-              },
-            },
-          })
-          .then((r) => {
-            bookObj.id = r.results[0].id;
-            bookObj.result = {
-              title: r.results[0].properties["이름"].title[0].plain_text,
-              author: r.results[0].properties["저자"].rich_text[0].plain_text,
-              category: r.results[0].properties["분야"].multi_select[0].name,
-              shelfNum: r.results[0].properties["책장 번호"].number,
-            };
+	reader.on("card", (card) => {
+		nfcObj.nfc = card.uid;
+		if (nfcObj.condition == "borrow") {
+			(async () => {
+				await notion.databases
+					.query({
+						database_id: bookDb,
+						filter: {
+							property: "NFC",
+							rich_text: {
+								equals: nfcObj.nfc,
+							},
+						},
+					})
+					.then((r) => {
+						bookObj.id = r.results[0].id;
+						bookObj.result = {
+							title: r.results[0].properties["제목"].title[0].plain_text,
+							author: r.results[0].properties["저자"].rich_text[0].plain_text,
+							category: r.results[0].properties["분야"].multi_select[0].name,
+							shelfNum: r.results[0].properties["책장 번호"].number,
+						};
 
-            mainWindow.webContents.send("scannedBook", bookObj);
-            nfcObj.condition = "";
-          })
-          .catch((e) => {
-            console.dir(e, { colors: true, depth: 20 });
-          });
-      })();
-    }
-    if (nfcObj.condition == "borrower") {
-      (async () => {
-        await notion.databases
-          .query({
-            database_id: personDb,
-            filter: {
-              property: "NFC",
-              rich_text: {
-                equals: nfcObj.nfc,
-              },
-            },
-          })
-          .then((r) => {
-            personId = r.results[0].properties["Person"].people[0].id;
-            pesonName = r.results[0].properties["Person"].people[0].name;
-            if (personId == "") {
-              return;
-            }
-            (async () => {
-              cancelResetEverything();
-              await notion.pages
-                .update({
-                  page_id: bookObj.id,
-                  properties: {
-                    "대출한 사람": {
-                      people: [
-                        {
-                          object: "user",
-                          id: personId,
-                        },
-                      ],
-                    },
-                    상태: {
-                      status: {
-                        name: "대출 중",
-                        color: "red",
-                      },
-                    },
-                  },
-                })
-                .then((r) => {
-                  mainWindow.webContents.send("done", {
-                    name: pesonName,
-                    ...bookObj,
-                  });
-                  resetEverything();
-                })
-                .catch((e) => {
-                  console.dir(e, { colors: true, depth: 20 });
-                });
-            })();
-          })
-          .catch((e) => {
-            console.dir(e, { colors: true, depth: 20 });
-          });
-      })();
-    }
+						mainWindow.webContents.send("scannedBook", bookObj);
+						nfcObj.condition = "";
+					})
+					.catch((e) => {
+						console.dir(e, { colors: true, depth: 20 });
+					});
+			})();
+		}
+		if (nfcObj.condition == "borrower") {
+			(async () => {
+				await notion.databases
+					.query({
+						database_id: personDb,
+						filter: {
+							property: "NFC",
+							rich_text: {
+								equals: nfcObj.nfc,
+							},
+						},
+					})
+					.then((r) => {
+						personId = r.results[0].properties["Person"].people[0].id;
+						pesonName = r.results[0].properties["Person"].people[0].name;
+						if (personId == "") {
+							return;
+						}
+						(async () => {
+							cancelResetEverything();
+							await notion.pages
+								.update({
+									page_id: bookObj.id,
+									properties: {
+										"대출한 사람": {
+											people: [
+												{
+													object: "user",
+													id: personId,
+												},
+											],
+										},
+										상태: {
+											status: {
+												name: "대출 중",
+												color: "red",
+											},
+										},
+									},
+								})
+								.then((r) => {
+									mainWindow.webContents.send("done", {
+										name: pesonName,
+										...bookObj,
+									});
+									resetEverything();
+								})
+								.catch((e) => {
+									console.dir(e, { colors: true, depth: 20 });
+								});
+						})();
+					})
+					.catch((e) => {
+						console.dir(e, { colors: true, depth: 20 });
+					});
+			})();
+		}
 
-    if (nfcObj.condition == "return") {
-      (async () => {
-        await notion.databases
-          .query({
-            database_id: bookDb,
-            filter: {
-              property: "NFC",
-              rich_text: {
-                equals: nfcObj.nfc,
-              },
-            },
-          })
-          .then(async (r) => {
-            bookObj.id = r.results[0].id;
-            bookObj.result = {
-              title: r.results[0].properties["이름"].title[0].plain_text,
-              author: r.results[0].properties["저자"].rich_text[0].plain_text,
-              category: r.results[0].properties["분야"].multi_select[0].name,
-              shelfNum: r.results[0].properties["책장 번호"].number,
-            };
-            await notion.pages
-              .update({
-                page_id: bookObj.id,
-                properties: {
-                  "대출한 사람": {
-                    people: [],
-                  },
-                  상태: {
-                    status: {
-                      name: "대출 가능",
-                      color: "green",
-                    },
-                  },
-                },
-              })
-              .then((r) => {
-                mainWindow.webContents.send("return", bookObj);
-                resetEverything();
-              })
-              .catch((e) => {
-                console.dir(e, { colors: true, depth: 20 });
-              });
-          })
-          .catch((e) => {
-            console.dir(e, { colors: true, depth: 20 });
-          });
-      })();
-    }
-  });
+		if (nfcObj.condition == "return") {
+			(async () => {
+				await notion.databases
+					.query({
+						database_id: bookDb,
+						filter: {
+							property: "NFC",
+							rich_text: {
+								equals: nfcObj.nfc,
+							},
+						},
+					})
+					.then(async (r) => {
+						bookObj.id = r.results[0].id;
+						bookObj.result = {
+							title: r.results[0].properties["제목"].title[0].plain_text,
+							author: r.results[0].properties["저자"].rich_text[0].plain_text,
+							category: r.results[0].properties["분야"].multi_select[0].name,
+							shelfNum: r.results[0].properties["책장 번호"].number,
+						};
+						await notion.pages
+							.update({
+								page_id: bookObj.id,
+								properties: {
+									"대출한 사람": {
+										people: [],
+									},
+									상태: {
+										status: {
+											name: "대출 가능",
+											color: "green",
+										},
+									},
+								},
+							})
+							.then((r) => {
+								mainWindow.webContents.send("return", bookObj);
+								resetEverything();
+							})
+							.catch((e) => {
+								console.dir(e, { colors: true, depth: 20 });
+							});
+					})
+					.catch((e) => {
+						console.dir(e, { colors: true, depth: 20 });
+					});
+			})();
+		}
+	});
 
-  reader.on("error", (err) => {
-    console.log(`NFC (${reader.reader.name}): an error occurred`, err);
-  });
-  reader.on("end", () => {
-    console.log(`NFC (${reader.reader.name}): device removed`);
-    if (mainWindow) {
-      mainWindow.webContents.send("remove-device", {
-        message: `NFC (${reader.reader.name}): device removed`,
-      });
-    }
-  });
+	reader.on("error", (err) => {
+		console.log(`NFC (${reader.reader.name}): an error occurred`, err);
+	});
+	reader.on("end", () => {
+		console.log(`NFC (${reader.reader.name}): device removed`);
+		if (mainWindow) {
+			mainWindow.webContents.send("remove-device", {
+				message: `NFC (${reader.reader.name}): device removed`,
+			});
+		}
+	});
 });
 
 nfc.on("error", (err) => {
-  console.log("NFC: an error occurred", err);
+	console.log("NFC: an error occurred", err);
 });
 
 function createWindow() {
-  // Create the browser window.
+	// Create the browser window.
 
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+	});
 
-  mainWindow.loadFile("index.html");
-  mainWindow.setMenu(null);
-  mainWindow.webContents.openDevTools();
-  // Emitted when the window is closed.
-  mainWindow.on("closed", function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+	mainWindow.loadFile("index.html");
+	mainWindow.setMenu(null);
+	mainWindow.webContents.openDevTools();
+	// Emitted when the window is closed.
+	mainWindow.on("closed", function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 }
 
 // This method will be called when Electron has finished
@@ -256,15 +256,15 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") app.quit();
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow();
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process
--- renderer.js
+++ renderer.js
@@ -3,48 +3,58 @@ let timerId;
 initiate();
 
 function initiate() {
-  bookObj = {};
-  $("#info").empty();
-  $("#buttons").empty();
-  $("#direction").empty();
-  $("#buttons").append(`					
+	bookObj = {};
+	$("#info").empty();
+	$("#buttons").empty();
+	$("#direction").empty();
+	$("#buttons").append(`					
 <button class="col-3 m-2  btn" id="borrow" style="min-width: 100px;">대출</button>
 <button class="col-3 m-2  btn" id="return" style="min-width: 100px;">반납</button>
 `);
 
-  document.getElementById("borrow").addEventListener("click", () => {
-    electronAPI.borrow();
-    $(`
+	$("#info").append(`					
+  <h1 class="title">
+  <span>*</span>
+  <span>*</span>
+  핀노드 도서 시스템
+  <span>*</span>
+  <span>*</span>
+</h1>
+`);
+
+	document.getElementById("borrow").addEventListener("click", () => {
+		electronAPI.borrow();
+		$(`
     <div class="m-5 card" style="display : none; max-height: 100px; min-width: 100px;">
       <div class="card-body notice" ">
       <p class="card-text font-weight-bold">책을 스캔해 주세요!</p>
     </div>
   </div>`)
-      .appendTo("#direction")
-      .show();
-  });
-  document.getElementById("return").addEventListener("click", () => {
-    electronAPI.return();
-    $(`
+			.appendTo("#direction")
+			.show();
+	});
+	document.getElementById("return").addEventListener("click", () => {
+		electronAPI.return();
+		$(`
     <div class="m-5 card" style="display : none; max-height: 100px; min-width: 100px;">
       <div class="card-body notice" ">
       <p class="card-text font-weight-bold">책을 스캔해 주세요!</p>
     </div>
   </div>`)
-      .appendTo("#direction")
-      .show();
-  });
+			.appendTo("#direction")
+			.show();
+	});
 }
 
 electronAPI.reset(function (e, dto) {
-  initiate();
+	initiate();
 });
 
 electronAPI.done(function (e, dto) {
-  $("#direction").empty();
-  $("#buttons").empty();
-  $("#info").empty();
-  $(`
+	$("#direction").empty();
+	$("#buttons").empty();
+	$("#info").empty();
+	$(`
   <div class="card text-center" style="display : none;">
     <div class="m-5 card-body" ">
     <p class="card-text font-weight-bold">감사합니다! ${dto.name}님</p>
@@ -53,20 +63,20 @@ electronAPI.done(function (e, dto) {
   </div>
   
 </div>`)
-    .appendTo("#direction")
-    .show();
-  timerId = setTimeout(initiate, 10000);
-  document.getElementById("confirm").addEventListener("click", function () {
-    clearTimeout(timerId);
-    initiate();
-  });
+		.appendTo("#direction")
+		.show();
+	timerId = setTimeout(initiate, 10000);
+	document.getElementById("confirm").addEventListener("click", function () {
+		clearTimeout(timerId);
+		initiate();
+	});
 });
 
 electronAPI.returnOn(function (e, dto) {
-  $("#direction").empty();
-  $("#buttons").empty();
-  $("#info").empty();
-  $(`
+	$("#direction").empty();
+	$("#buttons").empty();
+	$("#info").empty();
+	$(`
   <div class="card text-center" style="display : none;">
     <div class="m-5 card-body" ">
     <p class="card-text font-weight-bold">반납하신 책 : ${dto.result.title}</p>
@@ -74,32 +84,34 @@ electronAPI.returnOn(function (e, dto) {
   </div>
   
 </div>`)
-    .appendTo("#direction")
-    .show();
-  timerId = setTimeout(initiate, 10000);
-  document.getElementById("confirm").addEventListener("click", function () {
-    clearTimeout(timerId);
-    initiate();
-  });
+		.appendTo("#direction")
+		.show();
+	timerId = setTimeout(initiate, 10000);
+	document.getElementById("confirm").addEventListener("click", function () {
+		clearTimeout(timerId);
+		initiate();
+	});
 });
 
 electronAPI.scannedBook(function (e, dto) {
-  $("#direction").empty();
-  bookObj = dto.result;
-  electronAPI.borrower();
-  $("#return").remove();
-  $(
-    `<button class="col-3 m-2  btn" id="cancel" style="display : none; min-width: 100px;">취소</button>`
-  )
-    .appendTo("#buttons")
-    .show("slow", function () {
-      document
-        .getElementById("cancel")
-        .addEventListener("click", electronAPI.cancel);
-    });
-  $(`
+	$("#info").empty();
+	$("#direction").empty();
+	$("#buttons").empty();
+	bookObj = dto.result;
+	electronAPI.borrower();
+	$("#return").remove();
+	$(
+		`<button class="col-3 m-2  btn" id="cancel" style="display : none; min-width: 100px;">취소</button>`
+	)
+		.appendTo("#buttons")
+		.show("slow", function () {
+			document
+				.getElementById("cancel")
+				.addEventListener("click", electronAPI.cancel);
+		});
+	$(`
   <table class="table" style="display: none;">
-                
+
                   <h2>책 정보</h2>
                   <tbody>
                     <tr>
@@ -109,32 +121,32 @@ electronAPI.scannedBook(function (e, dto) {
                     <tr>
                       <th scope="row">지은이:</th>
                       <td id="author">${
-                        bookObj.author ? bookObj.author : "-"
-                      }</td>
+												bookObj.author ? bookObj.author : "-"
+											}</td>
   
                     </tr>
                     <tr>
                       <th scope="row">분야:</th>
                       <td id="category">${
-                        bookObj.category ? bookObj.category : "-"
-                      }</td>
+												bookObj.category ? bookObj.category : "-"
+											}</td>
   
                     </tr>
                     <tr>
                       <th scope="row">책장번호:</th>
                       <td id="borrower">${
-                        bookObj.shelfNum ? bookObj.shelfNum : "-"
-                      }</td>
+												bookObj.shelfNum ? bookObj.shelfNum : "-"
+											}</td>
                     </tr>
                   </tbody>
                 </table>`)
-    .appendTo("#info")
-    .show("slow");
-  $(`<div class="m-5 card" style="display : none; max-height: 100px; min-width: 100px;">
+		.appendTo("#info")
+		.show("slow");
+	$(`<div class="m-5 card" style="display : none; max-height: 100px; min-width: 100px;">
   <div class="card-body notice" ">
   <p class="card-text font-weight-bold">사원증을 스캔해 주세요!</p>
 </div>
 </div>`)
-    .appendTo("#direction")
-    .show();
+		.appendTo("#direction")
+		.show();
 });
